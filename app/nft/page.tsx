dc8c67ba882107f8df7577456864dbc9819ed69e
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-08-14 23:18:08 +0000

    fix navigation

---
+++ app/nft/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function NFTPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">NFTs</h1>
+      <p>Welcome to your NFT gallery. This page will display your NFTs and allow you to manage them.</p>
+    </div>
+  );
+}
