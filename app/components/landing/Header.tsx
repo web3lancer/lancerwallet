dee2a24e221359962d79d648cb2ff5a202cedeb3
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-08-14 21:59:34 +0000

    fix import

--- app/components/landing/Header.tsx
+++ app/components/landing/Header.tsx
@@ -1,7 +1,10 @@
+import Logo from "../Logo";
+
 export default function Header() {
   return (
     <header className="flex items-center justify-between py-6 px-6">
       <div className="flex items-center gap-3">
+        <Logo size={36} />
       </div>
       <div>
         <a href="/home" className="bg-purple-600 text-white px-4 py-2 rounded-lg shadow-sm hover:bg-purple-700">Enter App</a>
